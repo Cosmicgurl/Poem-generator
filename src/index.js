29a7134e6a5839eb6f4c6913c7bf3f11f5556a92
Cosmicgurl <165661217+Cosmicgurl@users.noreply.github.com>
2024-07-07 01:19:54 +0200

    Added axios and api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event){
-  event.preventDefault();
-
+function displayPoem(response){
   new Typewriter('#poem', {
-    strings: 'Roses are red, Violettes are blue',
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event){
+  event.preventDefault();
+
+  let promptElement = document.querySelector("#generated-poem")
+  let apiKey = "97bffc05t90b9a4d323obc7a7e7acf4e";
+  let prompt= `User instructions: Generate a poem about ${promptElement}`;
+  let context ="You are a romantic poem expert and you love to write short poem and seperate each line with a <br/>. Your mission is to generate a four line romantic poem. Make sure to use the user instructions. Sign the poem with the poet inside a <strong> element";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
+
+
 }
 
 
